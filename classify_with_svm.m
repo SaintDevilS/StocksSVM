9117739ae459299213b26f54cd3cb335c2dd721f
SaintDevilS <mooncake91@gmail.com>
2014-04-20 19:56:02 +0300

    did the classification only on intervals of month, and skipping every week, but the svmtrain isn't stopping

--- classify_with_svm.m
+++ classify_with_svm.m
@@ -2,39 +2,53 @@ function classify_with_svm()
     csvMatrix = csvread('C:\Weiyun\workspace\StocksSVM\vectors.csv');
     declare_global()
     
-    svmstruct = train_svm(csvMatrix);
+    svmstruct = train_svm(csvMatrix)
     
-    test_classify(csvMatrix, svmstruct)
+    %test_classify(csvMatrix, svmstruct)
 end
 
 function declare_global()
     global TRAINING_DATA_START TRAINING_DATA_END NUM_OF_FEATURES NUM_OF_STOCKS_FOR_TRAINING
 
     TRAINING_DATA_START = 719;
-    TRAINING_DATA_END = 11;
+    TRAINING_DATA_END = 30;
     NUM_OF_FEATURES = TRAINING_DATA_START - TRAINING_DATA_END + 1;
     NUM_OF_STOCKS_FOR_TRAINING = 160;
 end
 
-function svmstruct = train_svm(csvMatrix)
-    global TRAINING_DATA_START TRAINING_DATA_END NUM_OF_FEATURES NUM_OF_STOCKS_FOR_TRAINING
-    
-    training = zeros(NUM_OF_STOCKS_FOR_TRAINING ,NUM_OF_FEATURES);
-    for i = 1:NUM_OF_STOCKS_FOR_TRAINING
-        training(i,:) = csvMatrix(i,TRAINING_DATA_START:-1:TRAINING_DATA_END);
+function [current_interval_training, classification] = get_training_and_classification(csvMatrix, i, j, num_of_days_in_sample)
+    current_interval_training = csvMatrix(i,j:-1:j - num_of_days_in_sample + 1);
+        
+    if csvMatrix(i,j - num_of_days_in_sample) <= 0
+        classification = 'red';         
+    else
+        classification = 'green';
     end
 
-    classification = cell(NUM_OF_STOCKS_FOR_TRAINING ,1);
-    for i= 1:length(csvMatrix(1:NUM_OF_STOCKS_FOR_TRAINING ))
-        if csvMatrix(i,TRAINING_DATA_END - 1) <= 0
-            classification{i, 1} = 'red';
-        else
-            classification{i, 1} = 'green';
+end
+
+function svmstruct = train_svm(csvMatrix)
+    global TRAINING_DATA_START TRAINING_DATA_END NUM_OF_STOCKS_FOR_TRAINING
+    
+    num_of_days_in_sample = 30;
+    jump_interval = 7;
+    num_of_samples = floor(NUM_OF_STOCKS_FOR_TRAINING * (TRAINING_DATA_START - TRAINING_DATA_END) / jump_interval);
+    
+    training = zeros(num_of_samples ,num_of_days_in_sample);
+    classification = cell(num_of_samples ,1);
+    
+    index = 1;
+    for i = 1:NUM_OF_STOCKS_FOR_TRAINING
+        for j = TRAINING_DATA_START:-jump_interval:TRAINING_DATA_END
+            [training(index,:), classification{index, 1}] = get_training_and_classification(csvMatrix, i, j, num_of_days_in_sample);
+            
+            index=index + 1;
         end
     end
-  
-    svmstruct = svmtrain(training,classification, 'kernel_function', 'polynomial', 'polyorder', 4);
-    %svmstruct = svmtrain(training,classification);
+    
+    options = statset('MaxIter', 50000);
+    svmstruct = svmtrain(training,classification, 'kernel_function', 'polynomial', 'polyorder', 30, 'options', options);
+%    svmstruct = svmtrain(training,classification, 'options', options);
 end
 
 function test_classify(csvMatrix, svmstruct)
