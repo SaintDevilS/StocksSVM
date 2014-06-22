da0026926e563378ab7c03b3769d49e74e343f7d
SaintDevilS <mooncake91@gmail.com>
2014-06-23 00:07:27 +0300

    choosed parameters to pass to svmtrain based on the kernel function
    also fixed comparison of strings to strcmp

--- train_on_stocks_data.m
+++ train_on_stocks_data.m
@@ -1,11 +1,11 @@
 function path = train_on_stocks_data(training, classification, portion_of_data, kernel_function, poly_order, svmstruct_path)
-    if poly_order == ''
+    if strcmp(poly_order, '')
         svmstruct_name = sprintf('svm_struct_portion_%d_%s.mat', portion_of_data, kernel_function);
     else
         svmstruct_name = sprintf('svm_struct_portion_%d_order_%s.mat', portion_of_data, poly_order);
     end
     
-    if svmstruct_path ~= ''
+    if ~strcmp(svmstruct_path, '')
         path = strcat(strcat(svmstruct_path, '/'), svmstruct_name);
     else
         path = svmstruct_name;
@@ -17,8 +17,13 @@ function path = train_on_stocks_data(training, classification, portion_of_data, 
    
     options = statset('MaxIter', 4000000);
     
-    svmstruct = svmtrain(training(1:portion_of_data:length(training),:),classification(1:portion_of_data:length(classification)),...
-                         'kernelcachelimit', 15000,'options',options, 'kernel_function', 'mlp');
+    if strcmp(kernel_function, 'polynomial')
+        svmstruct = svmtrain(training(1:portion_of_data:length(training),:),classification(1:portion_of_data:length(classification)),...
+                             'options',options, 'kernel_function', kernel_function, 'polyorder', poly_order);
+    else
+        svmstruct = svmtrain(training(1:portion_of_data:length(training),:),classification(1:portion_of_data:length(classification)),...
+                             'options',options, 'kernel_function', kernel_function);                         
+    end
     
     save(path, 'svmstruct');
 end
