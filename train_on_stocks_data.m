a8057977e567650494bce3620157561f1b32b6e4
SaintDevilS <mooncake91@gmail.com>
2014-06-22 23:58:26 +0300

    added the option to select kernel_function
    - also, added the ability no to select path and curr folder would be selected
    in addition, fixed the name of the svmstruct

--- train_on_stocks_data.m
+++ train_on_stocks_data.m
@@ -1,7 +1,15 @@
-function path = train_on_stocks_data(training, classification, portion_of_data, poly_order, svmstruct_path)
-
-    svmstruct_name = sprintf('svm_struct_portion_%d_order_%s.mat', portion_of_data, poly_order);
-    path = strcat(strcat(svmstruct_path, '/'), svmstruct_name);
+function path = train_on_stocks_data(training, classification, portion_of_data, kernel_function, poly_order, svmstruct_path)
+    if poly_order == ''
+        svmstruct_name = sprintf('svm_struct_portion_%d_%s.mat', portion_of_data, kernel_function);
+    else
+        svmstruct_name = sprintf('svm_struct_portion_%d_order_%s.mat', portion_of_data, poly_order);
+    end
+    
+    if svmstruct_path ~= ''
+        path = strcat(strcat(svmstruct_path, '/'), svmstruct_name);
+    else
+        path = svmstruct_name;
+    end
     
     if exist(path, 'file')
         return;
@@ -10,7 +18,7 @@ function path = train_on_stocks_data(training, classification, portion_of_data, 
     options = statset('MaxIter', 4000000);
     
     svmstruct = svmtrain(training(1:portion_of_data:length(training),:),classification(1:portion_of_data:length(classification)),...
-                         'kernelcachelimit', 15000,'options',options, 'kernel_function', 'polynomial', 'polyorder', 4);
+                         'kernelcachelimit', 15000,'options',options, 'kernel_function', 'mlp');
     
     save(path, 'svmstruct');
 end
